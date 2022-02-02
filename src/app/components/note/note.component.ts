5b37b59d647097dfa4e38b870aee22309a5bf4c5
abaza738 <maher.abaza@protonmail.com>
2022-02-03 00:08:48 +0200

    Updated the `Note` component. Added styles for note type.

--- src/app/components/note/note.component.ts
+++ src/app/components/note/note.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-note',
@@ -6,8 +6,6 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./note.component.css']
 })
 export class NoteComponent implements OnInit {
-
-  note: any;
 
   constructor() { }
 
